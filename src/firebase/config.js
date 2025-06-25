50c33c0c9ff17c2464b8eaa68389d9438469187d
Shibilipadalath <shibilipadalath@gmail.com>
2025-06-25 15:41:08 +0530

    some update in firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,14 @@ import 'firebase/compat/firestore'
 import 'firebase/compat/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD9Ui6au4NHR_hSzMn3fctOmrbzXKZrUSQ",
-    authDomain: "olx-clone-69b69.firebaseapp.com",
-    projectId: "olx-clone-69b69",
-    storageBucket: "olx-clone-69b69.firebasestorage.app",
-    messagingSenderId: "429081211746",
-    appId: "1:429081211746:web:1e07b4735e9d8c6f2c323b",
-    measurementId: "G-7JXQSZN12P"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 export default firebase.initializeApp(firebaseConfig)
